09b5a52f5d99ce4ea41fa6a61e38ccd7a1114b59
Nik Silver <nik@niksilver.com>
2026-07-15 22:10:53 +0100

    Better score balance on 2-stripe cards.
    See design diary 2026-07-15: New numbering, plus playtest:
    evernote:///view/24342524/s202/ba108949-18d4-9ce1-a02c-473860c9ec61?ht=2026-07-15%3A%20New%20numbering%2C%20plus%20playtest#aa60e057-7d5e-482d-bc6b-c17edc9a0175/4528fda9-a404-7543-4402-8ffb515326b1

--- card_images.py
+++ card_images.py
@@ -238,9 +238,26 @@ def one_stripe_scores(include: list[bool]) -> str:
     Given a 1-stripe pattern, return the scores that such a card
     would have.
     """
-    scores = []
     idx = include.index(True)
-    return [idx+21, idx+21+5, idx+21+5+5, 45-5-idx, 45-idx]
+    scores = [idx+21, idx+21+5, idx+21+5+5, 45-5-idx, 45-idx]
+    return [str(s) for s in scores]
+
+
+def two_stripe_scores(include: list[bool]) -> str:
+    """
+    Given a 2-stripe pattern, return the scores that such a card
+    would have.
+    For details see design diary of 2026-07-15 (new numbering).
+    """
+    lookup = [[  [0, 0], [1, 20], [5, 16], [8, 13], [11, 12]],
+              [  [0, 0], [0,  0], [2, 19], [6, 15], [ 9, 10]],
+              [  [0, 0], [0,  0], [0,  0], [3, 18], [ 7, 14]],
+              [  [0, 0], [0,  0], [0,  0], [0,  0], [ 4, 17]],
+             ]
+    row = include.index(True)
+    col = include.index(True, row + 1)
+    scores = lookup[row][col]
+    return [str(s) for s in scores]
 
 
 # Set up the stripe images to use
@@ -266,14 +283,12 @@ cards = []
 
 for include in stripe_includes[1]:
     for score in one_stripe_scores(include):
-        card = make_card(str(score), include)
+        card = make_card(score, include)
         cards.append(card)
 
 # 2-stripe cards
 
-for count in range(0, 20):
-    include_idx = count % len(stripe_includes[2])
-    include     = stripe_includes[2][include_idx]
-    score = str(count + 1)
-    crd   = make_card(score, include)
-    cards.append(crd)
+for include in stripe_includes[2]:
+    for score in two_stripe_scores(include):
+        card = make_card(score, include)
+        cards.append(card)
